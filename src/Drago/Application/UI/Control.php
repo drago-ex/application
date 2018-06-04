138b692abbf776148b8897e80ec44c7214583cf2
Zdeněk Papučík <zdenek.papucik@gmail.com>
2018-06-04 13:17:57 +0200

    Create Control.php

---
+++ src/Drago/Application/UI/Control.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Drago Application
+ * Copyright (c) 2015, Zdeněk Papučík
+ */
+namespace Drago\Application\UI;
+
+use Nette\Application\UI;
+use stdClass;
+
+/**
+ * Base control.
+ */
+class Control extends UI\Control
+{
+	use Drago;
+
+	/**
+	 * Is AJAX request?
+	 * @return bool
+	 */
+	public function isAjax()
+	{
+		return $this->presenter->isAjax();
+	}
+
+	/**
+	 * Saves the message to template.
+	 * @param string $message
+	 * @param string $type
+	 * @return stdClass
+	 */
+	public function flashMessage($message, $type = 'info')
+	{
+		parent::flashMessage($message, $type);
+		return $this->presenter->flashMessage($message, $type);
+	}
+
+}
