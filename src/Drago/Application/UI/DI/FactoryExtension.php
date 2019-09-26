d476d19f3d725ddfd4301d70cc229245792864d1
accgit <zdenek.papucik@gmail.com>
2019-09-26 13:35:24 +0200

    Update

---
+++ src/Drago/Application/UI/DI/FactoryExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Application
+ * Package built on Nette Framework
+ */
+namespace Drago\Application\UI;
+
+use Nette\DI\CompilerExtension;
+
+
+/**
+ * Register service.
+ * @package Drago\Application
+ */
+class FactoryExtension extends CompilerExtension
+{
+	public function loadConfiguration(): void
+	{
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('Factory'))
+			->setFactory(Factory::class);
+	}
+}
