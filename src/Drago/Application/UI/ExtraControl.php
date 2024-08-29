ddfa2607ce9c2ae32efce025865e8ff99e35ba7b
Zdeněk Papučík <zdenek.papucik@gmail.com>
2024-08-29 06:53:34 +0200

    Update ExtraControl.php

--- src/Drago/Application/UI/ExtraControl.php
+++ src/Drago/Application/UI/ExtraControl.php
@@ -20,8 +20,6 @@ use Nette\Localization\Translator;
  */
 class ExtraControl extends Control
 {
-	public const Offcanvas = 'Offcanvas';
-	public const Modal = 'Modal';
 	public ?Translator $translator = null;
 
 
@@ -51,14 +49,5 @@ class ExtraControl extends Control
 	{
 		$factory = $form[$component];
 		return $factory instanceof BaseControl ? $factory : null;
-	}
-
-
-	/**
-	 * Returns a fully-qualified name that uniquely identifies the component within the presenter hierarchy.
-	 */
-	public function getUniqueIdComponent(string $name): string
-	{
-		return $this->getUniqueId() . $name;
 	}
 }
