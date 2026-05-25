f0806eaea9d50bd9de39cf027399c4e548ca3baa
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-25 07:50:10 +0200

    Update

--- src/Drago/Application/UI/ExtraTemplate.php
+++ src/Drago/Application/UI/ExtraTemplate.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 
 namespace Drago\Application\UI;
 
+use Nette\Application\UI\Presenter;
 use Nette\Bridges\ApplicationLatte\Template;
+use Nette\Forms\Control;
+use Nette\Security\User;
 use stdClass;
 
 
 /** Base template class with common properties for presenters and controls. */
 class ExtraTemplate extends Template
 {
+	public Presenter $presenter;
+	public Control $control;
+	public User $user;
+	public string $baseUrl;
+	public string $basePath;
+
 	/** @var array<int, stdClass> */
 	public array $flashes = [];
 }
